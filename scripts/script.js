12a001218b803bd02c73d21176b99c08491efc1f
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2024-09-23 12:56:08 +0530

    completed: sidebar

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  const scrollY = window.scrollY;
+  document.getElementById("nav-container").style.display =
+    scrollY >= 280 ? "none" : "block";
+  // console.log(scrollY);
+};
+
+
+function sidebarToggle(){
+
+    let sidebar = document.getElementById("sidebar")
+    
+    sidebar.classList.toggle("d-none")
+    
+
+}
